c22f4dfddfa1f60a854a7cf936369669e9ebfd97
unknown <emrecanpinar06@gmail.com>
2020-12-26 20:00:13 +0300

    i2c buffer name changed

--- Core/Inc/module/i2c/i2c_read_write.c
+++ Core/Inc/module/i2c/i2c_read_write.c
@@ -11,16 +11,16 @@
 
 
 uint8_t write8(I2C_HandleTypeDef *huart,uint8_t device,uint8_t reg,uint8_t val){
-	buffer[0] = reg;
-	buffer[1] = val;
-	HAL_I2C_Master_Transmit(huart, device, buffer, 2, I2C_TRANSMIT_TIMEOUT);
+	buffer_i2c[0] = reg;
+	buffer_i2c[1] = val;
+	HAL_I2C_Master_Transmit(huart, device, buffer_i2c, 2, I2C_TRANSMIT_TIMEOUT);
 
 	return 1;
 }
 
 uint8_t read8(I2C_HandleTypeDef *huart,uint8_t device,uint8_t reg){
-	buffer[0] = reg;
-	HAL_I2C_Master_Transmit(huart, device, buffer, 1, I2C_TRANSMIT_TIMEOUT);
-	HAL_I2C_Master_Receive(huart, device, &buffer[2], 1, I2C_RECEIVE_TIMEOUT);
-	return buffer[2];
+	buffer_i2c[0] = reg;
+	HAL_I2C_Master_Transmit(huart, device, buffer_i2c, 1, I2C_TRANSMIT_TIMEOUT);
+	HAL_I2C_Master_Receive(huart, device, &buffer_i2c[1], 1, I2C_RECEIVE_TIMEOUT);
+	return buffer_i2c[1];
 }
